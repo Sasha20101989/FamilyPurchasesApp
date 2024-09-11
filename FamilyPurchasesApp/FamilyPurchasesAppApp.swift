1da6fecab48be28af13964c7d9537f163f6cbfbe
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2024-09-11 20:59:42 +0300

    Initial Commit

---
+++ FamilyPurchasesApp/FamilyPurchasesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyPurchasesAppApp.swift
+//  FamilyPurchasesApp
+//
+//  Created by Aleksander on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FamilyPurchasesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
